df4a7d5795cdb1ecbd847ef017f0043d4b202cd5
Astratus <Austin.w.huddleson@gmail.com>
2022-01-01 22:12:24 -0800

    Now we have something half functional...

--- Astrigraph.sh
+++ Astrigraph.sh
@@ -11,60 +11,67 @@
 
 # Define add_exention_to_context function
 # Given a Content and Extention. Make a list of uniqe connections.
-# Keep track of two arrays. Link arrays based array Index. Loop through them testing if connection already exists.
+# Keep track of two arrays. Link arrays based on array Index. Loop through them testing if connection already exists.
 function add_extention_to_context {
   local Context=$1
   local Extention=$2
   local AlreadyAdded=0
+  local ArrayIndexes
 
   # If the given context doesn't have any connections yet, add connection.
+  echo "trying to add $Extention to $Context"
   if [[ ! " ${linkcontextarray[*]} " =~ " ${Context} " ]]; then
-    linkcontextarray+=("$Context")
-    linkextentionarray+=("$Extention")
+    echo "Context has no connections just adding it"
+    linkcontextarray+=("${Context}")
+    linkextentionarray+=("${Extention}")
 
   # Given context has atleast one connection already. Check if connection already exists.
   else
-
+    echo "Context already has connections"
+    echo "checking ${linkcontextarray[@]} for ${Context}"
     # Get a list of array indexes where context already exists.
-    # For each value already in linkcontextarray
     for i in "${!linkcontextarray[@]}"; do
 
+    echo "Checking if ${linkcontextarray[$i]} = ${Context}"
     # Where the given Context is already in the array add it's array index number to ArrayIndexes.
-    if [[ "${linkcontextarray[$i]}" == "${Context}" ]]; then
-       ArrayIndexes+="${i}"
+    if [[ ${linkcontextarray[$i]} == ${Context} ]]; then
+      echo "${linkcontextarray[$i]} = ${Context} index is ${i}"
+      ArrayIndexes+=("${i}")
+     else
+       echo "${linkcontextarray[$i]} does not = ${Context}"
     fi
+    done
 
+    echo "${linkcontextarray[@]} contains ${Context} at all these indexes ${ArrayIndexes[@]}"
     # For each index number in ArrayIndexes
-    for i in "${ArrayIndexes[@]}"
+    for x in "${!ArrayIndexes[@]}"
     do
-
+      echo "Checking if ${linkextentionarray[$x]} is equal to ${Extention}"
       # check if corrisponding linkextentionarray matches given Exention. if true set AlreadySet.
-      if [[ "$linkextentionarray[$i]" == "$Exention" ]]; then
-
+      if [[ "${linkextentionarray[$x]}" == "${Extention}" ]]; then
+        echo "${linkextentionarray[$x]} is equal to ${Extention}"
         # Extention/node already mapped to Context/Cluster
         AlreadyAdded=1
       fi
-    # Done looking through linkextentionarray
-    done
-
-    # Done looking through linkcontextarray
     done
 
     # If AlreadyAdded is not 1. Add it.
-    if [[ "$AlreadyAdded" != 1 ]]; then
+    if [[ "${AlreadyAdded}" == 0 ]]; then
+      echo "Adding ${Extention} to $Context"
       linkcontextarray+=("$Context")
       linkextentionarray+=("$Extention")
-
-    # Reset AlreadyAdded for next run.
-    else
-      AlreadyAdded=0
+      echo "${linkcontextarray[@]} shoud now contain $Context"
+      echo "${linkextentionarray[@]} should now contain $Extention"
     fi
   fi
   }
 
+# For gosub and goto processing. Given source context & source Extention & Con Params & ConType
+# ConParams are the params given to the gosub or goto. In other words it's What's in the ()
+# ConType is set to either "gosub" or "goto" accordingly
 function add_Go_connection_to_graph {
 local SourceContext=$1
-local SourceExention=$2
+local SourceExtention=$2
 local ConParams=$3
 local ConType=$4
 
@@ -80,64 +87,60 @@ case ${#JustCommas} in
 
   1)
   #echo "Connecting to another exention in the same context"
-  ConSourceContext+=("$SourceContext")
-  ConSourceExtention+=("$SourceExention")
-  ConDestContext+=("$SourceContext")
-  ConDestExtention+=`echo $ConParams | cut -d ',' -f 1`
-  ConCommand+=$ConType
+  ConSourceContext+=("${SourceContext}")
+  ConSourceExtention+=("${SourceExtention}")
+  ConDestContext+=("${SourceContext}")
+  ConDestExtention+=(`echo ${ConParams} | cut -d ',' -f 1`)
+  ConCommand+=${ConType}
   ;;
 
   2)
   #echo "Connecting to another exention in a differnet context"
-  ConSourceContext+=("$SourceContext")
-  ConSourceExtention+=("$SourceExention")
-  ConDestContext+=`echo $ConParams | cut -d ',' -f 1`
-  ConDestExtention+=`echo $ConParams | cut -d ',' -f 2`
-  ConCommand+=$ConType
+  ConSourceContext+=("${SourceContext}")
+  ConSourceExtention+=("${SourceExtention}")
+  ConDestContext+=(`echo ${ConParams} | cut -d ',' -f 1`)
+  ConDestExtention+=(`echo ${ConParams} | cut -d ',' -f 2`)
+  ConCommand+=(${ConType})
   ;;
 
 esac
-}
-
-function add_GotoIf_To_graph {
-  local SourceContext=$1
-  local SourceExention=$2
-  local ConParams=$3
-
-
-
 }
 
 # Get the array index of a connection. Since we are using the array index as the node name, we need to be able to look this up.
 function get_index_of_connection {
 local Context=$1
 local Extention=$2
-
-for i in "${!linkcontextarray[@]}"; do
+local ArrayIndexes
 
 # Where the given Context is already in the array add it's array index number to ArrayIndexes.
-if [[ "${linkcontextarray[$i]}" == "${Context}" ]]; then
-   ArrayIndexes+="${i}"
-fi
+for i in "${!linkcontextarray[@]}"; do
+  if [[ "${linkcontextarray[$i]}" == "${Context}" ]]; then
+    ArrayIndexes+="$i"
+  fi
+done
 
-# For each index number in ArrayIndexes
-for i in "${ArrayIndexes[@]}"
-do
-
-  # check if corrisponding linkextentionarray matches given Exention. if true set AlreadySet.
-  if [[ "$linkextentionarray[$i]" == "$Exention" ]]; then
-
+# For each index number in ArrayIndexes.
+# Check if corrisponding linkextentionarray matches given Exention. if true set AlreadySet.
+for i in "${!ArrayIndexes[@]}"; do
+  if [[ "${linkextentionarray[$i]}" == "${Extention}" ]]; then
     # Return index of connection
     return $i
   fi
-# Done looking through linkextentionarray
-done
-
-# Done looking through linkcontextarray
 done
 }
 
-# echo every line one at a time
+# For gotoif processing. Given source context & source extention & ConParams.
+# ConParams are the params given to the gotoif. In other words it's What's in the ()
+function add_GotoIf_To_graph {
+  local SourceContext=$1
+  local SourceExtention=$2
+  local ConParams=$3
+# Need to finish
+
+}
+# End of Functions
+
+# line one at a time. Line is stored in $p
 while read p; do
 
 	# Get the first char from the line
@@ -147,7 +150,7 @@ while read p; do
 	First3Char=${p::3}
 
 	# Based on the char of the first char of the line...
-	case $FirstChar in
+	case ${FirstChar} in
 
 	# If the first char is a open bracket, this must be a context declaration.
 	\[)
@@ -156,20 +159,20 @@ while read p; do
 
     # if this context name is not already in the contextarray then add it.
 		if [[ ! " ${contextarray[*]} " =~ " ${context} " ]]; then
-		contextarray+=("$context")
+		contextarray+=("${context}")
 		fi
 		;;
 
 	# if the first char is a semi colon check if start of comment block. Set var InCommentBlock accordingly.
 	\;)
 		# if the first 3 chars are ";--" set the InComment var so we know we are in a comment block.
-    if [[ $First3Char = "\;--" ]]; then
+    if [[ ${First3Char} = "\;--" ]]; then
 			InCommentBlock=1
 		fi
 		;;
 
   # if the first charactor is a "-" check if end of a comment block. Set var InCommentBlock accordingly.
-  -)
+  \-)
     if [[ $First3Char = "--\;" ]]; then
       InCommentBlock=0
     fi
@@ -177,72 +180,90 @@ while read p; do
 
   # If the first charactor is an "e" this must be the start of a extention declaration
 	e)
-
+    echo "e processing for $p"
     # Set var exten to the exention.
     # cut off "exten" and "=>" | cut off everything after the first ","
 		exten=`echo $p | cut -d " " -f 3 | cut -d "," -f 1`
+    echo "Exten is $exten in context $context"
 
     # Add extention to contexts
-    add_extention_to_context $context $exten
+    add_extention_to_context ${context} ${exten}
 
     # Check if line contains ",Goto("
-		if  [[ ! -z `echo $p | grep -i ',Goto('` ]]; then
+		if  [[ "$p" == *",Goto("* ]]; then
+      echo "goto processing $p"
+      #Grab everything after goto and remove ()
       ConParams=`echo $p | awk 'BEGIN{FS="Goto"}{print $2}' | sed 's/(//g' | sed 's/)//g'`
       ConType="Goto"
+
+      echo "Context is $context exten is $exten ConType is $ConType con params are $ConParams"
+
       add_Go_connection_to_graph $context $exten $ConParams $ConType
     fi
 
     # Check if line contains ",GotoIf("
-    if  [[ ! -z `echo $p | grep -i ',GotoIf('` ]]; then
-      ConParams=`echo $p | awk 'BEGIN{FS="GotoIf"}{print $2}'`
+    if  [[ "$p" == *",GotoIf("* ]]; then
+      echo "gotoif processing $p"
 
-      # Get length of ConParams minus 1
-      LenConParams=${#ConParams}
-      echo "$ConParams is $LenConParams long in $p"
-      LenConParams=$(expr $LenConParams - 1)
+      # Get everything after GotoIf(
+      ConParams=`echo $p | awk 'BEGIN{FS="GotoIf\("}{print $2}'`
 
       # Strip last char of string which should be the )
-      ConParams=${ConParams:1:$LenConParams}
+      LenConParams=${#ConParams}
+      #echo "$ConParams is $LenConParams long in $p"
+      LenConParams=$(expr ${LenConParams} - 1)
+      ConParams=${ConParams:0:${LenConParams}}
 
       add_GotoIf_To_graph $context $exten $ConParams
     fi
 
     # Check if line contains ",Gosub("
-    if [[ ! -z `echo $p | grep -i ',Gosub('` ]]; then
+    if [[ "$p" == *",Gosub("* ]]; then
+      echo "gosub processing $p"
+      # Grab everything after gosub and remove ()
       ConParams=`echo $p | awk 'BEGIN{FS="Gosub"}{print $2}' | sed 's/(//g' | sed 's/)//g'`
       ConType="Gosub"
+
       add_Go_connection_to_graph $context $exten $ConParams $ConType
     fi
 
 		;;
 
 	s)
+  echo "s processing fpr $p"
   # Check if line contains ",Goto("
-  if  [[ ! -z `echo $p | grep -i ',Goto('` ]]; then
+  if  [[ "$p" == *",Goto("* ]]; then
+    echo "Goto processing for $p"
+    #Grab everything after goto and remove ()
     ConParams=`echo $p | awk 'BEGIN{FS="Goto"}{print $2}' | sed 's/(//g' | sed 's/)//g'`
     ConType="Goto"
+    echo "Context is $context exten is $exten ConType is $ConType con params are $ConParams"
+
     add_Go_connection_to_graph $context $exten $ConParams $ConType
   fi
 
   # Check if line contains ",GotoIf("
-  if  [[ ! -z `echo $p | grep -i ',GotoIf('` ]]; then
+  if  [[ "$p" == *",GotoIf("* ]]; then
+    echo "gotoif processing for $p"
+    # Get everything after GotoIf(
     ConParams=`echo $p | awk 'BEGIN{FS="GotoIf"}{print $2}'`
 
-    # Get length of ConParams minus 1
-    LenConParams=${#ConParams}
-    echo "$ConParams is $LenConParams long in $p"
-    LenConParams=$(expr $LenConParams - 1)
-
     # Strip last char of string which should be the )
-    ConParams=${ConParams:1:$LenConParams}
+    LenConParams=${#ConParams}
+    #echo "$ConParams is $LenConParams long in $p"
+    LenConParams=$(expr $LenConParams - 1)
+    ConParams=${ConParams:0:$LenConParams}
 
     add_GotoIf_To_graph $context $exten $ConParams
   fi
 
   # Check if line contains ",Gosub("
-  if [[ ! -z `echo $p | grep -i ',Gosub('` ]]; then
+  if [[ "$p" == *",Gosub("* ]]; then
+    echo "Gosub processing for $p"
+    # Grab everything after gosub and remove ()
     ConParams=`echo $p | awk 'BEGIN{FS="Gosub"}{print $2}' | sed 's/(//g' | sed 's/)//g'`
     ConType="Gosub"
+
     add_Go_connection_to_graph $context $exten $ConParams $ConType
   fi
 		;;
@@ -265,9 +286,11 @@ sed -i 's/GoToIf/GotoIf/g' extentions.conf
 sed -i 's/goToif/GotoIf/g' extentions.conf
 sed -i 's/gotoIf/GotoIf/g' extentions.conf
 sed -i 's/gotoif/GotoIf/g' extentions.conf
+sed -i 's/GotoIF/GotoIf/g' extentions.conf
+sed -i 's/GotoIf/GotoIf/g' extentions.conf
 
 # Load extentions.conf into this mess and get going.
-done <extentions.conf
+done < extentions.conf
 
 # echo all contexts from array
 # echo ${contexts[*]}
@@ -277,20 +300,20 @@ echo "digraph astrigraph {" > graph.dot
 echo " " >> graph.dot
 
 # For each context, create a subgraph
-for i in "${contextarray[@]}"; do
+for i in "${!contextarray[@]}"; do
 
   # Start subgraph with style and label
-  echo "subgraph $contextarray[$i] {" >> graph.dot
-  echo "label = \"$contextarray[$i]\"" >> graph.dot
+  echo "subgraph cluster_${i} {" >> graph.dot
+  echo "label = \"${contextarray[$i]}\";" >> graph.dot
   echo "style=filled;" >> graph.dot
   echo "color=lightgrey;" >> graph.dot
   echo "node [style=filled,color=white];" >> graph.dot
 
   # For each value in linkcontext array. Check if context name matches, if so add array index (to be the node name) and the exention (to be the node label).
   # The reason we are using the index as the node name is because node names must be uniqe accross all subgraphs.
-  for x in "$linkcontextarray[@]"; do
-    if [[ "$linkcontextarray[$x]" == "$contextarray[$i]" ]]; then
-      echo "$x [label=$linkextentionarray[$x] ];" >> graph.dot
+  for x in "${!linkcontextarray[@]}"; do
+    if [[ "${linkcontextarray[$x]}" == "${contextarray[$i]}" ]]; then
+      echo "$x [label=\"${linkextentionarray[$x]}\"];" >> graph.dot
     fi
   done
 
@@ -303,28 +326,34 @@ done
 echo " " >> graph.dot
 
 # Create edges/connectionsfor inner context connections first
-for i in "$contextarray[@]"; do
-  for x in "$ConSourceContext[@]"; do
-  #  if [[ "$contextarray[$i]" == "$ConSourceContext[$x]" ]] && [[ "$contextarray[$i]" == "$ConDestContext[$x]" ]]; then
-      get_index_of_connection $ConSourceContext[$x] $ConSourceExtention[$x]
+for i in "${!contextarray[@]}"; do
+  for x in "${!ConSourceContext[@]}"; do
+    if [[ ${contextarray[$i]} == ${ConSourceContext[$x]} ]] && [[ ${contextarray[$i]} == ${ConDestContext[$x]} ]]; then
+      get_index_of_connection ${ConSourceContext[$x]} ${ConSourceExtention[$x]}
       ConSource=$?
-      get_index_of_connection $ConDestContext[$x] $ConDestExtention[$x]
+      get_index_of_connection ${ConDestContext[$x]} ${ConDestExtention[$x]}
       ConDest=$?
-      echo "$ConSource --> $ConDest;" >> graph.dot
-  #  fi
+      echo "${ConSource} --> ${ConDest};" >> graph.dot
+    fi
   done
 done
 
 # Create edges/connections for all other connections
-for i in "$contextarray[@]"; do
-  for x in "$ConSourceContext[@]"; do
-    if [[ "$contextarray[$i]" == "$ConSourceContext[$x]" ]] && [[ "$contextarray[$i]" != "$ConDestContext[$x]" ]]; then
-      get_index_of_connection $ConSourceContext[$x] $ConSourceExtention[$x]
+for i in "${!contextarray[@]}"; do
+  for x in "${!ConSourceContext[@]}"; do
+    if [[ ${contextarray[$i]} == ${ConSourceContext[$x]} ]] && [[ ${contextarray[$i]}" != "${ConDestContext[$x]} ]]; then
+      get_index_of_connection ${ConSourceContext[$x]} ${ConSourceExtention[$x]}
       ConSource=$?
-      get_index_of_connection $ConDestContext[$x] $ConDestExtention[$x]
+      get_index_of_connection ${ConDestContext[$x]} ${ConDestExtention[$x]}
       ConDest=$?
       echo "$ConSource --> $ConDest;" >> graph.dot
     fi
   done
 done
 echo '}' >> graph.dot
+
+
+echo "contextarray is ${contextarray[@]}"
+echo "ConSourceContext is ${ConsourceContext[@]}"
+echo "linkcontextarray is ${linkcontextarray[@]}"
+echo "linkextentionarray is ${linkextentionarray[@]}"
